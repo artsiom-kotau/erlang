c9cfe1892263042db09a0d2bad7e39654abd516e
Artsiom Kotau <artsiom.kotau@gmail.com>
2016-04-03 13:40:46 +0300

    Merge sort works on base tests

--- algorithms/insertion_sort/src/main/merge_sort.erl
+++ algorithms/insertion_sort/src/main/merge_sort.erl
@@ -8,9 +8,26 @@
 %%%-------------------------------------------------------------------
 -module(merge_sort).
 -author("roodxx").
-
+-import(list_api, [list_size/1, list_split/2]).
 %% API
--export([]).
+-export([merge_sort/1]).
+
+merge_sort([H | T]) ->
+  Length = list_size([H | T]),
+  do_sort([H | T], Length div 2);
+merge_sort([]) -> [].
+
+do_sort([H | T], Part) ->
+  if Part > 0 ->
+    {LL, RL} = list_split([H | T], Part),
+    LP = Part div 2,
+    RP = Part - LP,
+    LLM = do_sort(LL, LP),
+    RLM = do_sort(RL, RP),
+    merge(LLM, RLM);
+    true -> [H | T]
+  end;
+do_sort([], _) -> [].
 
 merge([FH | FT], [SH | ST]) ->
   if FH < SH -> [FH | merge(FT, [SH | ST])];
---
+++ algorithms/insertion_sort/src/test/merge_sort_test.erl
@@ -0,0 +1,32 @@
+%%%-------------------------------------------------------------------
+%%% @author roodxx
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 03. крс 2016 13.06
+%%%-------------------------------------------------------------------
+-module(merge_sort_test).
+-author("roodxx").
+
+-include_lib("eunit/include/eunit.hrl").
+
+sort_empty_list_test() -> ?assert(merge_sort:merge_sort([]) == []).
+
+sort_single_item_list_test() -> ?assert(merge_sort:merge_sort([1]) == [1]).
+
+sort_two_same_items_list_test() -> ?assert(merge_sort:merge_sort([1,1]) == [1,1]).
+
+sort_three_same_items_list_test() -> ?assert(merge_sort:merge_sort([1,1,1]) == [1,1,1]).
+
+sort_two_non_same_items_straight_order_test() -> ?assert(merge_sort:merge_sort([1,2]) == [1,2]).
+
+sort_two_non_same_items_reverse_order_test() -> ?assert(merge_sort:merge_sort([2,1]) == [1,2]).
+
+sort_three_non_same_items_straight_order_test() -> ?assert(merge_sort:merge_sort([1,2,3]) == [1,2,3]).
+
+sort_three_non_same_items_reverse_order_test() -> ?assert(merge_sort:merge_sort([3,2,1]) == [1,2,3]).
+
+sort_three_non_same_items_random_order_test() -> ?assert(merge_sort:merge_sort([3,1,2]) == [1,2,3]).
+
+sort_four_non_same_items_random_order_test() -> ?assert(merge_sort:merge_sort([3,1,4,2]) == [1,2,3,4]).
