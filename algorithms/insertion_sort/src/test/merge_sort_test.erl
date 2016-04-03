f57135e30a6036330fe5d3ef6bad2005bdd66446
Artsiom Kotau <artsiom.kotau@gmail.com>
2016-04-03 22:24:04 +0300

    Merge sort works on all tests

--- algorithms/insertion_sort/src/test/merge_sort_test.erl
+++ algorithms/insertion_sort/src/test/merge_sort_test.erl
@@ -30,3 +30,7 @@ sort_three_non_same_items_reverse_order_test() -> ?assert(merge_sort:merge_sort(
 sort_three_non_same_items_random_order_test() -> ?assert(merge_sort:merge_sort([3,1,2]) == [1,2,3]).
 
 sort_four_non_same_items_random_order_test() -> ?assert(merge_sort:merge_sort([3,1,4,2]) == [1,2,3,4]).
+
+sort_random_order_even_amount_test() -> ?assert(merge_sort:merge_sort([2,29,87,56,68,61,37,20,7,74]) == [2,7,20,29,37,56,61,68,74,87]).
+
+sort__random_order_odd_amount_test() -> ?assert(merge_sort:merge_sort([2,29,56,68,61,37,20,7,74]) == [2,7,20,29,37,56,61,68,74]).
