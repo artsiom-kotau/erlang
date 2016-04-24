091a819c88a31393a6119443279933d357edb45e
Artsiom Kotau <artsiom.kotau@gmail.com>
2016-04-24 23:29:19 +0300

    Add whole tests

--- algorithms/insertion_sort/src/test/max_subarray_test.erl
+++ algorithms/insertion_sort/src/test/max_subarray_test.erl
@@ -20,8 +20,14 @@ single_positive_item_array_test() ->
 single_negative_item_array_test() ->
   ?assert(-1 =:= max_subarray:max_subarray([-1])).
 
-multiple_positive_item_array_test() ->
+multiple_positive_items_array_test() ->
   ?assert(6 =:= max_subarray:max_subarray([1,2,3])).
 
-multiple_negative_item_array_test() ->
+multiple_negative_items_array_test() ->
   ?assert(-1 =:= max_subarray:max_subarray([-1,-2])).
+
+multiple_different_items_array1_test() ->
+  ?assert(6 =:= max_subarray:max_subarray([-2, 1, -3, 4, -1, 2, 1, -5, 4])).
+
+multiple_different_items_array2_test() ->
+  ?assert(7 =:= max_subarray:max_subarray([-2, -3, 4, -1, -2, 1, 5, -3])).
